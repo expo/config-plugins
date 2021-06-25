06b25e0a5fe6e3271f41c363e660f9998423d94c
Evan Bacon <baconbrix@gmail.com>
2021-06-25 15:49:53 -0600

    Improved demo

--- apps/app/src/react-native-dynamic-app-icon/App.tsx
+++ apps/app/src/react-native-dynamic-app-icon/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { useState } from "react";
-import { Text, Button, View } from "react-native";
-
-import AppIcon from "react-native-dynamic-app-icon";
+import Entypo from '@expo/vector-icons/Entypo';
+import React from 'react';
+import { Image, SafeAreaView, Text, View } from 'react-native';
+import AppIcon from 'react-native-dynamic-app-icon';
+import TouchableBounce from 'react-native/Libraries/Components/Touchable/TouchableBounce';
 
 function useIconName() {
     const [icon, _setIcon] = React.useState(null);
@@ -15,31 +15,145 @@ function useIconName() {
         return () => (isMounted = false);
     }, []);
 
-    const setIcon = React.useCallback((icon) => {
-        AppIcon.setAppIcon(icon);
-        _setIcon(icon)
-    }, [_setIcon])
-    return [icon, setIcon];
+    const setIcon = React.useCallback(
+        (icon) => {
+            AppIcon.setAppIcon(icon);
+            _setIcon(icon || null);
+        },
+        [_setIcon]
+    );
+    return [icon === "default" ? null : icon, setIcon];
 }
 
-export default function App() {
-    const [icon, setIcon] = useIconName();
-    const [index, setIndex] = React.useState(0)
-    const icons = [null, '0', '1', '2'];
+const icons = [
+    {
+        name: "Automatic",
+        source: require("../../assets/icon.png"),
+        iconId: null,
+        color: "#FFBB8E",
+    },
+    {
+        name: "Spring",
+        source: require("../../assets/stickers/avocool.png"),
+        iconId: "0",
+        color: "#A2A3F1",
+    },
+    {
+        name: "Summer",
+        source: { uri: "https://pbs.twimg.com/profile_images/1308332115919020032/jlqFOD33_400x400.jpg", },
+        color: "#FFDB8A",
+        iconId: "1",
+    },
+    {
+        name: "Autumn",
+        source: { uri: "https://s3.amazonaws.com/assets.materialup.com/users/pictures/000/361/144/preview/open-uri20180613-4-1yz52gq?1528892052" },
+        color: "#FFBB8E",
+        iconId: "2",
+    },
+];
 
-    React.useEffect(() => {
-        setIcon(icons[index % icons.length]);
-    }, [index]);
+export default function App() {
+    const [_icon, setIcon] = useIconName();
 
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Current: {icon}</Text>
-            <Button
-                onPress={() => {
-                    setIndex(() => index + 1);
+        <SafeAreaView
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <View style={{ width: "100%" }}>
+                {icons.map((icon, index) => {
+                    return (
+                        <Item
+                            onPress={() => {
+                                setIcon(icon.iconId);
+                            }}
+                            isSelected={icon.iconId === _icon}
+                            name={icon.name}
+                            color={icon.color}
+                            source={icon.source}
+                            key={String(index)}
+                        />
+                    );
+                })}
+            </View>
+        </SafeAreaView>
+    );
+}
+
+function Item({ ...props }) {
+    return (
+        <TouchableBounce
+            onPress={props.onPress}
+            style={{ marginVertical: 8, marginHorizontal: 24 }}
+        >
+            <View
+
+                style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "stretch",
+                    shadowColor: "#000",
+                    shadowOffset: {
+                        width: 0,
+                        height: 4,
+                    },
+                    shadowOpacity: props.isSelected ? 0.35 : 0.2,
+                    shadowRadius: 8.46,
+
+                    backgroundColor: props.isSelected ? "#000" : "#F1F1F1",
+                    elevation: 9,
+                    paddingHorizontal: 12,
+                    paddingVertical: 12,
+                    borderRadius: 20,
                 }}
-                title="Toggle Icon"
-            />
-        </View>
+            >
+                <View
+                    style={{
+                        flexDirection: "row",
+                        flex: 1,
+                        alignItems: "center",
+                    }}
+                >
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            flex: 1,
+                            alignItems: "center",
+                        }}
+                    >
+                        <Image
+                            source={props.source}
+                            style={{
+                                width: 64,
+                                aspectRatio: 1,
+                                resizeMode: "cover",
+                                borderRadius: 16,
+                            }}
+                        />
+                        <Text
+                            style={{
+                                marginLeft: 20,
+                                fontSize: 18,
+                                fontWeight: "700",
+                                color: props.isSelected ? "white" : "black",
+                            }}
+                        >
+                            {props.name}
+                        </Text>
+                    </View>
+                    {props.isSelected && (
+                        <Entypo
+                            style={{ marginRight: 8 }}
+                            name="check"
+                            size={20}
+                            color="white"
+                        />
+                    )}
+                </View>
+            </View>
+        </TouchableBounce>
     );
 }
