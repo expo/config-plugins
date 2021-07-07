d96c0b2888da3808d7c4e2dd7993129711ba717d
Fernando Rojo <frojo@sas.upenn.edu>
2021-07-07 23:32:09 +0000

    docs: add android-jsc-intl example

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -3,3 +3,4 @@ export { default } from './src/react-native-dynamic-app-icon/App';
 // export { default } from './src/detox/App';
 // export { default } from './src/react-native-ble-plx/App';
 // export { default } from './src/react-native-webrtc/App';
+// export { default } from './src/android-jsc-intl/App';
