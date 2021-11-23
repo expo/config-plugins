81857f893899724e2e1bf3289d8d25dbc864311d
Evan Bacon <bacon@expo.io>
2021-11-23 15:37:03 -0700

    Added CI updates

--- apps/app/index.js
+++ apps/app/index.js
@@ -1,7 +1,16 @@
 import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 
-import App from "./App";
+import Constants from "expo-constants";
+
+let App;
+// Only use the Detox test in CI
+if ((Constants.manifest.extra || {}).CI) {
+  App = require("./src/detox/App").default;
+} else {
+  App = require("./App").default;
+}
+// import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
