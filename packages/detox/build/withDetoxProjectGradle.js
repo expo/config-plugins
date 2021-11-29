3ac42ac2faec93aad18d24f02ed61426c8f2a4f8
Evan Bacon <bacon@expo.io>
2021-11-29 16:19:48 -0700

    Support monorepos in detox maven gradle import

--- packages/detox/build/withDetoxProjectGradle.js
+++ packages/detox/build/withDetoxProjectGradle.js
@@ -1,11 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.setGradleMaven = void 0;
+exports.addDetoxImport = void 0;
 const config_plugins_1 = require("@expo/config-plugins");
+const generateCode_1 = require("@expo/config-plugins/build/utils/generateCode");
 // Because we need the package to be added AFTER the React and Google maven packages, we create a new allprojects.
 // It's ok to have multiple allprojects.repositories, so we create a new one since it's cheaper than tokenizing
-// the existing block to find the correct place to insert our Detox maven.
-const gradleMaven = 'allprojects { repositories { maven { url "$rootDir/../node_modules/detox/Detox-android" } } }';
+// the existing block to find the correct place to insert our camera maven.
+const gradleMaven = [
+    `def detoxMavenPath = new File(["node", "--print", "require.resolve('detox/package.json')"].execute(null, rootDir).text.trim(), "../Detox-android")`,
+    `allprojects { repositories { maven { url(detoxMavenPath) } } }`,
+].join("\n");
 /**
  * [Step 3](https://github.com/wix/Detox/blob/master/docs/Introduction.Android.md#3-add-the-native-detox-dependency) Add detox to the project build.gradle.
  * @param config
@@ -13,7 +17,7 @@ const gradleMaven = 'allprojects { repositories { maven { url "$rootDir/../node_
 const withDetoxProjectGradle = (config) => {
     return config_plugins_1.withProjectBuildGradle(config, (config) => {
         if (config.modResults.language === "groovy") {
-            config.modResults.contents = setGradleMaven(config.modResults.contents);
+            config.modResults.contents = addDetoxImport(config.modResults.contents).contents;
         }
         else {
             throw new Error("Cannot add Detox maven gradle because the project build.gradle is not groovy");
@@ -21,13 +25,35 @@ const withDetoxProjectGradle = (config) => {
         return config;
     });
 };
-function setGradleMaven(buildGradle) {
-    // If this specific line is present, skip.
-    // This also enables users in bare workflow to comment out the line to prevent detox from adding it back.
-    if (buildGradle.includes("detox/Detox-android")) {
-        return buildGradle;
-    }
-    return buildGradle + `\n${gradleMaven}\n`;
+function addDetoxImport(src) {
+    return appendContents({
+        tag: "detox-import",
+        src,
+        newSrc: gradleMaven,
+        comment: "//",
+    });
 }
-exports.setGradleMaven = setGradleMaven;
+exports.addDetoxImport = addDetoxImport;
+// Fork of config-plugins mergeContents, but appends the contents to the end of the file.
+function appendContents({ src, newSrc, tag, comment, }) {
+    const header = generateCode_1.createGeneratedHeaderComment(newSrc, tag, comment);
+    if (!src.includes(header)) {
+        // Ensure the old generated contents are removed.
+        const sanitizedTarget = generateCode_1.removeGeneratedContents(src, tag);
+        const contentsToAdd = [
+            // @something
+            header,
+            // contents
+            newSrc,
+            // @end
+            `${comment} @generated end ${tag}`,
+        ].join("\n");
+        return {
+            contents: sanitizedTarget !== null && sanitizedTarget !== void 0 ? sanitizedTarget : src + contentsToAdd,
+            didMerge: true,
+            didClear: !!sanitizedTarget,
+        };
+    }
+    return { contents: src, didClear: false, didMerge: false };
+}
 exports.default = withDetoxProjectGradle;
