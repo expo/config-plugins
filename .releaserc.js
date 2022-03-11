414087e68b38a5c1aab6813c213b919ee5742d59
Evan Bacon <bacon@expo.io>
2022-03-11 14:14:35 -0700

    feat(ci): added auto release actions to GitHub Actions

---
+++ .releaserc.js
@@ -0,0 +1,74 @@
+// From @byCedric https://github.com/expo/expo-github-action/blob/main/.releaserc.js
+
+const rules = [
+  { type: "feat", release: "minor", title: "New features" },
+  { type: "feature", release: "minor", title: "New features" },
+  { type: "fix", release: "patch", title: "Bug fixes" },
+  { type: "refactor", release: "patch", title: "Code changes" },
+  { type: "chore", release: "patch", title: "Other chores" },
+  { type: "docs", release: "patch", title: "Documentation changes" },
+];
+
+// Simple mapping to order the commit groups
+const sortMap = Object.fromEntries(
+  rules.map((rule, index) => [rule.title, index])
+);
+
+module.exports = {
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "next",
+    "next-major",
+    {
+      name: "beta",
+      prerelease: true,
+    },
+    {
+      name: "alpha",
+      prerelease: true,
+    },
+  ],
+  tagFormat: "${version}",
+  repositoryUrl: "https://github.com/expo/config-plugins.git",
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { breaking: true, release: "major" },
+          { revert: true, release: "patch" },
+        ].concat(
+          rules.map(({ type, release, breaking }) => ({
+            type,
+            release,
+            breaking,
+          }))
+        ),
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: rules.map(({ type, title }) => ({ type, section: title })),
+        },
+        writerOpts: {
+          commitGroupsSort: (a, z) => sortMap[a.title] - sortMap[z.title],
+        },
+      },
+    ],
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        message: "chore: ${nextRelease.version}\n\n${nextRelease.notes}",
+        assets: ["package.json", "CHANGELOG.md", "build/*"],
+      },
+    ],
+    "@semantic-release/github",
+    "@semantic-release/npm",
+  ],
+};
---
+++ CONTRIBUTING.md
@@ -0,0 +1,15 @@
+# Contributing to Expo Config Plugins
+
+## Submitting a pull request
+
+To submit a pull request:
+
+1. Fork the [repository](https://github.com/expo/config-plugins) and create a feature branch. (Existing contributors can create feature branches without forking. Prefix the branch name with `@your-github-username/`.)
+2. Use [conventional commits](https://www.conventionalcommits.org/en/v1.0.0/) including in the PR tile.
+3. Fill out the entire PR template. Ensure your code is tested locally and has continuous tests in CI.
+4. Make sure all tests pass on GitHub Actions.
+5. Wait for a review and adjust the code if necessary.
+
+## Publishing a release
+
+All publishing should be handled automatically whenever code is merged to `main`.
