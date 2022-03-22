dd8f606de3570e613fa4ca6bb79e16d86dec47a1
evanbacon <bacon@expo.io>
2022-03-22 16:49:33 -0500

    chore(android-jsc-intl): lint fix

--- packages/android-jsc-intl/src/withAndroidJscIntl.ts
+++ packages/android-jsc-intl/src/withAndroidJscIntl.ts
@@ -2,22 +2,24 @@ import { ConfigPlugin, withAppBuildGradle } from "@expo/config-plugins";
 
 /**
  * Apply android-jsc-intl configuration for Expo SDK 42 projects.
- * 
+ *
  * This plugin lets you access the `Intl` API in Android apps (without Hermes).
  */
 const withAndroidJscIntl: ConfigPlugin = (config) => {
   // Return the modified config.
-  return withAppBuildGradle(config, config => {
-    if (config.modResults.language === 'groovy') {
+  return withAppBuildGradle(config, (config) => {
+    if (config.modResults.language === "groovy") {
       config.modResults.contents = config.modResults.contents.replace(
-        'org.webkit:android-jsc:+',
-        'org.webkit:android-jsc-intl:+'
-      )
+        "org.webkit:android-jsc:+",
+        "org.webkit:android-jsc-intl:+"
+      );
     } else {
-      throw new Error('[@expo/config-plugins][withAndroidJscIntl] Cannot enable Intl in Android JSC app gradle because the build.gradle is not groovy.')
+      throw new Error(
+        "[@expo/config-plugins][withAndroidJscIntl] Cannot enable Intl in Android JSC app gradle because the build.gradle is not groovy."
+      );
     }
-    return config
+    return config;
   });
 };
 
-export default withAndroidJscIntl
+export default withAndroidJscIntl;
