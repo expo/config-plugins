9bad93b466c3cb4ed8fd7c6d9faa819f42caff50
evanbacon <bacon@expo.io>
2023-07-14 14:55:37 -0500

    drop unused config files

--- apps/ios-stickers/metro.config.js
+++
@@ -1,6 +0,0 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("@expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-module.exports = config;
