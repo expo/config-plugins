17fa3083e85866cf76d87b2e0dcf1644cc6ea775
Gabriel Taveira <gabrielstaveira@gmail.com>
2023-11-03 15:15:20 -0300

    docs(react-native-code-push): actually use code push on the example app

--- apps/react-native-code-push/index.js
+++ apps/react-native-code-push/index.js
@@ -1,5 +1,11 @@
 import { registerRootComponent } from "expo";
+import codePush from "react-native-code-push";
 
 import App from "./src/App";
 
-registerRootComponent(App);
+const appWithCodePush = codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_START,
+  installMode: codePush.InstallMode.IMMEDIATE,
+})(App);
+
+registerRootComponent(appWithCodePush);
