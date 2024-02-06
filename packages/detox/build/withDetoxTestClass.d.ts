45115c3ff066db5548ca4f1c622798a6a134a8ec
evanbacon <bacon@expo.io>
2024-02-05 20:19:50 -0600

    drop build folder

--- packages/detox/build/withDetoxTestClass.d.ts
+++
@@ -1,5 +0,0 @@
-import { ConfigPlugin } from "expo/config-plugins";
-/**
- * [Step 5](https://github.com/wix/Detox/blob/master/docs/Introduction.Android.md#5-create-a-detox-test-class). Create `DetoxTest.java`
- */
-export declare const withDetoxTestClass: ConfigPlugin;
--- packages/detox/build/withKotlinGradle.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigPlugin } from "expo/config-plugins";
-/**
- * Lifted from [unimodules-test-core](https://github.com/expo/expo/blob/master/packages/unimodules-test-core/app.plugin.js).
- *
- * @param config Expo config
- * @param version Kotlin version to use
- */
-declare const withKotlinGradle: ConfigPlugin<string>;
-export default withKotlinGradle;
