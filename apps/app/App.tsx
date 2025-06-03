00197c766b131ef4ca58d12eefbf2f67a7d2197a
Brent Vatne <brentvatne@gmail.com>
2025-06-02 17:07:45 -0700

    Remove @config-plugins/detox

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,3 +1,2 @@
-export { default } from "./src/detox/App";
-// export { default } from "./src/android-jsc-intl/App";
+export { default } from "./src/android-jsc-intl/App";
 // export { default } from "./src/react-native-adjust/App";
--- packages/detox/app.plugin.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("./build/withDetox");
